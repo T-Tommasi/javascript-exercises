fbc1fb634899ac6897fba3f56e708e8c12a6a874
T-Tommasi <ttommasi1945@gmail.com>
2024-04-17 20:12:52 +0200

    (feat) solved sumAll exercise
    NPM test not working due to incompatibility with Node.JS 8.5.5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,9 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(startNumber,finalNumber) {
+    let total = 0;
+    for (i = 0; i < finalNumber; i++) {
+        total += startNumber+i
+    }
+    return total
+}
 // Do not edit below this line
 module.exports = sumAll;
